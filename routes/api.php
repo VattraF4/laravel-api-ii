014ebf20dd8e854110125bc058c37757aab0c248
VattraF4 <vattraramo13@gmail.com>
2026-08-31 00:58:47 +0700

    52.Loggin Users out and delete token:
    feat: add logout functionality to AuthController and update routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -41,6 +42,10 @@ Route::prefix('posts')->middleware('auth:sanctum')->group(function () {
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
-    Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 });
