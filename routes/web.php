25958c2503c546fb7611d605c3875269c25ab883
VattraF4 <vattraramo13@gmail.com>
2026-09-16 10:58:10 +0700

    66.Helper Function part.1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostWebController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,16 @@ Route::get('/page', function () {
 Route::resources([
     'posts-web' => PostWebController::class,
 ]);
+
+Route::get('/helper', function () {
+    $user = User::find(2);
+    echo optional($user)->verified_at; // Optional helper (Laravel 5.5+)
+    $user?->verified_at; // Null safety operator , better than optional helper (PHP 8+)
+    echo "<br>";
+
+    echo str('Laravel Framework')->upper();
+    echo "<br> " . str('Laravel Framework')->slug();
+    echo "<br> " . str('Laravel')->contains('ara'); // true or false , 1 or 0
+    echo "<br> " . str('Hello World')->limit(5);
+    echo "<br> " . str('Hello World')->limit(5)->append(' <b>see more</b>');
+});
